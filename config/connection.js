28a85bc0aa57ea525bdf5e06dc8b898b2a6f88d4
Robert VanScoy <rvans003@fiu.edu>
2021-05-01 12:55:43 -0400

    added .env file to project with SQL Database Root password. Added connection settings in connection.js and referenced .env variables.

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,19 @@
-// initial commit
+require('dotenv').config()
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+
+  port: 3306,
+
+
+  user: 'root',
+
+
+  password: process.env.DB_PASS,
+  database: 'department_db',
+});
+
+module.exports = connection;
